c3bb222864794f7e38f406d0adf878007ef5dc6c
bielmenezesc <gabriel.cabral@ccc.ufcg.edu.br>
2023-08-20 23:33:30 -0300

    Melhorando teste da classe boleto

--- processadorBoletos/src/test/java/com/mycompany/app/BoletoTest.java
+++ processadorBoletos/src/test/java/com/mycompany/app/BoletoTest.java
@@ -1,15 +1,17 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class BoletoTest {
 
     @Test
-    public void testImprimirMensagem() {
-        Boleto boleto = new Boleto("xxxxxxxxxx", "17/03/2001", "R$549,49");
+    public void testBoleto() {
+        Boleto boleto = new Boleto("xxxxxxxxxx", "17/03/2001", 549.49);
+        
+        // Verificando se a instância não é nula
         assertNotNull(boleto);
-    }
 
 }
