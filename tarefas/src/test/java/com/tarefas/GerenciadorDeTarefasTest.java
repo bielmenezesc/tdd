183a39a6c458bbeb02e0ebafa25d24b26bdb461f
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2023-08-21 18:19:25 -0300

    Criando teste para gerar lista ordenada por data e prioridade
    red bar

--- tarefas/src/test/java/com/tarefas/GerenciadorDeTarefasTest.java
+++ tarefas/src/test/java/com/tarefas/GerenciadorDeTarefasTest.java
@@ -117,4 +117,16 @@ public class GerenciadorDeTarefasTest {
         assertEquals(tarefa1.getTitulo(), lista.get(0).getTitulo());
         assertEquals(tarefa2.getTitulo(), lista.get(1).getTitulo());
     }
+
+    @Test
+    public void listarTarefasOrdenadasPorDataDeVencimentoEPrioridade() {
+        Tarefa tarefa1 = new Tarefa("Titulo1", "Descricao1", LocalDate.parse("2011-11-11"), PrioridadeTarefa.BAIXA);
+        Tarefa tarefa2 = new Tarefa("Titulo2", "Descricao2", LocalDate.parse("2011-11-11"), PrioridadeTarefa.MEDIA);
+        gerenciador.adicionarTarefa(tarefa1);
+        gerenciador.adicionarTarefa(tarefa2);
+        ArrayList<Tarefa> lista = gerenciador.listarTarefasOrdenadasPorDataDeVencimentoEPrioridade();
+
+        assertEquals(tarefa2.getTitulo(), lista.get(0).getTitulo());
+        assertEquals(tarefa1.getTitulo(), lista.get(1).getTitulo());
+    }
 }
