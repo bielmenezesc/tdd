ef27b92014690c2440237a90193818f5965bb7f0
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2023-08-21 16:20:03 -0300

    Criando listarTarefas() - green bar

---
+++ tarefas/src/main/java/com/tarefas/GerenciadorDeTarefas.java
@@ -0,0 +1,9 @@
+package com.tarefas;
+
+import java.util.ArrayList;
+
+public class GerenciadorDeTarefas {
+    public ArrayList<Integer> listarTarefas() {
+        return new ArrayList<>();
+    }
+}
