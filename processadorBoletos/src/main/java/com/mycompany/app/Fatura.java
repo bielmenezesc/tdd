9c69d604b468c006b73e591d3285e872af287c65
bielmenezesc <gabriel.cabral@ccc.ufcg.edu.br>
2023-08-21 08:35:50 -0300

    Criando classe Processador e refatorando fatura para passar no teste

--- processadorBoletos/src/main/java/com/mycompany/app/Fatura.java
+++ processadorBoletos/src/main/java/com/mycompany/app/Fatura.java
@@ -5,11 +5,17 @@ public class Fatura {
     private String data;
     Double valor;
     private String nomeCliente;
+    String situacao;
 
     public Fatura(String data, Double valor, String nomeCliente) {
         this.data = data;
         this.valor = valor;
         this.nomeCliente = nomeCliente;
+        this.situacao = "NAO PAGA";
+    }
+
+    public void setFaturaPaga() {
+       this.situacao = "PAGA";
     }
 
 }
