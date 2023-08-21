99a90a8d11c1fab6f60afdd76c9841ad80795f25
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2023-08-21 16:55:25 -0300

    Criando construtor, propriedades, getters - green bar

--- tarefas/src/main/java/com/tarefas/Tarefa.java
+++ tarefas/src/main/java/com/tarefas/Tarefa.java
@@ -1,4 +1,33 @@
 package com.tarefas;
 
+import java.time.LocalDate;
+
 public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataDeVencimento;
+    private PrioridadeTarefa prioridade;
+
+    public Tarefa() {
+        this.titulo = "";
+        this.descricao = "";
+        this.dataDeVencimento = null;
+        this.prioridade = PrioridadeTarefa.BAIXA;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return this.dataDeVencimento;
+    }
+
+    public PrioridadeTarefa getPrioridade() {
+        return this.prioridade;
+    }
 }
