b24e7f227570b09596d443bebc8ebac40f46f2b5
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2023-08-21 16:57:47 -0300

    Testando criacao de tarefa com detalhes - red bar

--- tarefas/src/test/java/com/tarefas/TarefaTest.java
+++ tarefas/src/test/java/com/tarefas/TarefaTest.java
@@ -2,6 +2,8 @@ package com.tarefas;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 public class TarefaTest {
@@ -12,5 +14,14 @@ public class TarefaTest {
         assertEquals("", tarefa.getDescricao());
         assertEquals(null, tarefa.getDataDeVencimento());
         assertEquals(PrioridadeTarefa.BAIXA, tarefa.getPrioridade());
+    }
+
+    @Test
+    public void novaTarefa_ComDetalhes() {
+        Tarefa tarefa = new Tarefa("Titulo", "Descricao", LocalDate.parse("2023-09-09"), PrioridadeTarefa.ALTA);
+        assertEquals("Titulo", tarefa.getTitulo());
+        assertEquals("Descricao", tarefa.getDescricao());
+        assertEquals(LocalDate.parse("2023-09-09"), tarefa.getDataDeVencimento());
+        assertEquals(PrioridadeTarefa.ALTA, tarefa.getPrioridade());
     }
 }
