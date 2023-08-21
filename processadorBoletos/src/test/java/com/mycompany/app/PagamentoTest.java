47a77aa0cfd082346c23efe0c1279b9d069f8879
bielmenezesc <gabriel.cabral@ccc.ufcg.edu.br>
2023-08-21 09:12:30 -0300

    Refatorando teste pagamento

--- processadorBoletos/src/test/java/com/mycompany/app/PagamentoTest.java
+++ processadorBoletos/src/test/java/com/mycompany/app/PagamentoTest.java
@@ -9,7 +9,9 @@ import org.junit.Test;
 public class PagamentoTest {
     @Test
     public void testPagamento() {
-        Pagamento pagamento = new Pagamento(549.49, LocalDate.now(), "BOLETO");
+        Pagamento pagamento = new Pagamento(549.49, LocalDate.now(), "BOLETO", 
+        new Boleto("123456789", LocalDate.now(), 150.50),
+        new Fatura(LocalDate.now(), 549.49, "Gabriel Menezes Cabral"));
         
         // Verificando se a instância não é nula
         assertNotNull(pagamento);
