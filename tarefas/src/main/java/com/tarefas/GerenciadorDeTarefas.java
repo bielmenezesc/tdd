d0d4028d817ce7fd2249a1122d1190842108a73d
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2023-08-21 16:37:54 -0300

    Criando novos metodos e construtor - green bar

--- tarefas/src/main/java/com/tarefas/GerenciadorDeTarefas.java
+++ tarefas/src/main/java/com/tarefas/GerenciadorDeTarefas.java
@@ -3,7 +3,17 @@ package com.tarefas;
 import java.util.ArrayList;
 
 public class GerenciadorDeTarefas {
-    public ArrayList<Integer> listarTarefas() {
-        return new ArrayList<>();
+    private ArrayList<Tarefa> tarefas;
+
+    public GerenciadorDeTarefas() {
+        this.tarefas = new ArrayList<Tarefa>();
+    }
+
+    public ArrayList<Tarefa> listarTarefas() {
+        return tarefas;
+    }
+
+    public void adicionarTarefa(Tarefa tarefa) {
+        this.tarefas.add(tarefa);
     }
 }
