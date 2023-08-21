0d11616c55dbc9432d7b70f7f4c4218929b0a4fe
bielmenezesc <gabriel.cabral@ccc.ufcg.edu.br>
2023-08-21 10:35:22 -0300

    Melhorando testes do processador de boletos

--- processadorBoletos/src/test/java/com/mycompany/app/ProcessadorTest.java
+++ processadorBoletos/src/test/java/com/mycompany/app/ProcessadorTest.java
@@ -6,26 +6,41 @@ import java.time.LocalDate;
 
 import org.junit.Test;
 
-public class ProcessadorTest 
-{
+public class ProcessadorTest {
  
-    @Test
-    public void testProcessador() {
-
-        Boleto[] boletos = {
+    Boleto[] boletos1 = {
         new Boleto("123456789", LocalDate.now(), 150.50), 
         new Boleto("987654321", LocalDate.now(), 75.25), 
         new Boleto("555888222", LocalDate.now(), 1000.00),
         new Boleto("777999444", LocalDate.now(), 250.75)
-        };
+    };
 
-        Fatura fatura = new Fatura(LocalDate.now(), 549.49, "Gabriel Menezes Cabral");
+    Boleto[] boletos2 = {
+        new Boleto("123456789", LocalDate.now(), 150.50), 
+        new Boleto("987654321", LocalDate.now(), 75.25), 
+        new Boleto("555888222", LocalDate.now(), 10.00),
+        new Boleto("777999444", LocalDate.now(), 250.75)
+    };
 
+    Fatura fatura = new Fatura(LocalDate.now(), 549.49, "Gabriel Menezes Cabral");
+
+    @Test
+    public void testFaturaPaga() {
+        
         Processador processador = new Processador();
-
-        processador.processarBoletos(boletos, fatura);
+        processador.processarBoletos(boletos1, fatura);
 
         assertEquals(fatura.situacao, "PAGA");
+
+    }
+
+    @Test
+    public void testFaturaNaoPaga() {
+        
+        Processador processador = new Processador();
+        processador.processarBoletos(boletos2, fatura);
+
+        assertEquals(fatura.situacao, "NAO PAGA");
 
     }
 }
